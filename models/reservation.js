de2058d3ccaeb2509185515c1ea18e7f8a730172
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-10-26 16:19:01 -0500

    saving reservations

--- models/reservation.js
+++ models/reservation.js
@@ -3,6 +3,7 @@
 const moment = require("moment");
 
 const db = require("../db");
+const { get } = require("./customer");
 
 
 /** A reservation for a party */
@@ -81,6 +82,33 @@ class Reservation {
     );
 
     return results.rows.map(row => new Reservation(row));
+  }
+
+  /** find a reservation by id. */
+
+  static async get(id) {
+    const result = await db.query(`SELECT id, customer_id AS "customerId", num_guests AS "numGuests", start_at AS "startAt", notes FROM reservations WHERE id = $1`, [id]);
+
+    let reservation = result.row[0];
+
+    if (reservation === undefined) {
+      const err = new Error(`No such reservation; ${id}`);
+      err.status = 404;
+      throw err;
+    }
+
+    return new Reservation(reservation);
+  }
+
+  /** save reservation */
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(`INSERT INTO reservations (customer_id, num_guests, start_at, notes) VALUES ($1, $2, $3, $4) RETURNING id`, [this.customerId, this.numGuests, this.startAt, this.notes]);
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(`UPDATE reservations SET num_guests=$1, start_at=$2, notes=$3 WHERE id=$4`, [this.numGuests, this.startAt, this.notes, this.id]);
+    }
   }
 }
 
